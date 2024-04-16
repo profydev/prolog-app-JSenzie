aeca3cc7a6fb4c72ba358ae02ca7ccbf94188ea0
joseph <jsensback@gmail.com>
2024-04-16 13:39:21 -0700

    added all buttons

--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -1,9 +1,44 @@
-import { ButtonHTMLAttributes } from "react";
+import React from "react";
 import classNames from "classnames";
 import styles from "./button.module.scss";
+import { ButtonHTMLAttributes } from "react";
 
-export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+export enum ButtonSize {
+  sm = "sm",
+  md = "md",
+  lg = "lg",
+  xl = "xl",
+}
+
+export enum ButtonColor {
+  primary = "primary",
+  secondary = "secondary",
+  gray = "gray",
+  empty = "empty",
+  emptyGray = "empty-gray",
+  error = "error",
+  emptyError = "empty-error",
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  size?: ButtonSize;
+  color?: ButtonColor;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export function Button({
+  children,
+  size = ButtonSize.md,
+  color = ButtonColor.primary,
+}: ButtonProps) {
   return (
-    <button {...props} className={classNames(styles.button, props.className)} />
+    <div>
+      <button
+        className={classNames(styles.button, styles[size], styles[color])}
+      >
+        {children}
+      </button>
+    </div>
   );
 }
