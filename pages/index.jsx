76ed2aa1fde3609ff028cf49daef659ca3402ae2
Joseph Sensback <101530708+JosephSensback@users.noreply.github.com>
2025-02-16 12:22:31 -0800

    final (#24)

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,60 @@
-import { Routes } from "@config/routes";
+import { Button } from "@features/ui";
 import styles from "./index.module.scss";
+import { useState } from "react";
+import classNames from "classnames";
+import Link from "next/link";
 
 const IssuesPage = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
   return (
-    <div>
+    <div className={styles.issuesPage}>
       <header className={styles.header}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="/icons/logo-large.svg" alt="Prolog logo" />
-        <a href={Routes.projects}>Dashboard</a>
+        <div className={styles.links}>
+          <Link href="/">Home</Link>
+          <Link href="/products">Products</Link>
+          <Link href="/documentation">Documentation</Link>
+          <Link href="/pricing">Pricing</Link>
+        </div>
+
+        <a href={"/dashboard"} className={styles.link}>
+          <Button>Open Dashboard</Button>
+        </a>
+        <Button
+          onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+          className={styles.menuButton}
+        >
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={isMobileMenuOpen ? "/icons/x.png" : "/icons/menu-2.png"}
+            alt={isMobileMenuOpen ? "close menu" : "open menu"}
+            className={styles.menuIcon}
+          />
+        </Button>
       </header>
+      <div
+        className={classNames(
+          styles.menuOverlay,
+          isMobileMenuOpen && styles.isMobileMenuOpen,
+        )}
+      />
+      <nav
+        className={classNames(
+          styles.nav,
+          isMobileMenuOpen && styles.isMobileMenuOpen,
+        )}
+      >
+        <div className={styles.sideLinks}>
+          <Link href="/">Home</Link>
+          <Link href="/products">Products</Link>
+          <Link href="/documentation">Documentation</Link>
+          <Link href="/pricing">Pricing</Link>
+        </div>
+        <a href={"/dashboard"} className={styles.sideDashboardLink}>
+          <Button>Open Dashboard</Button>
+        </a>
+      </nav>
       <button
         className={styles.contactButton}
         onClick={() =>
